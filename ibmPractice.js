9ad9e779e8af6ac8d2e212e350b3e45d41c40fea
naushinrahman <130614154+naushinrahman@users.noreply.github.com>
2023-10-10 14:55:33 -0400

    divide-array-into-2-subsets

---
+++ ibmPractice.js
@@ -0,0 +1,43 @@
+ 
+ function subsetA(arr) {
+     for (var i = 0; i < arr.length; i++) { 
+         for (var j = 0; j < (arr.length - i - 1); j++) { 
+             if (arr[j] < arr[j + 1]) { 
+                 var temp = arr[j] 
+                 arr[j] = arr[j + 1] 
+                 arr[j + 1] = temp 
+             } 
+         } 
+     } 
+     var sum = arr.reduce((accumulator, currentValue) => accumulator + currentValue);
+     console.log(sum)
+     var subA = arr[0] + arr[1];
+     console.log(subA)
+     var counter = 2;
+     if ((subA) > (sum - [subA])) {
+         var subSetA = arr.slice(0, 2);
+     } 
+     else {
+         while((subA) < (sum - [subA])) {
+             subA += arr[counter];
+             counter++;
+         }
+         var subSetA = arr.slice(0, counter);
+     }
+     for (var i = 0; i < subSetA.length; i++) { 
+        for (var j = 0; j < (subSetA.length - i - 1); j++) { 
+            if (arr[j] > subSetA[j + 1]) { 
+                var temp = subSetA[j] 
+                subSetA[j] = subSetA[j + 1] 
+                subSetA[j + 1] = temp 
+            } 
+        } 
+    } 
+     return subSetA;
+ }
+
+//  console.log(subsetA([3,7,5,6,2]));
+//  console.log(subsetA([5,3,2,4,1,2]));
+//  console.log(subsetA([6,1,5,2,4])); 
+ console.log(subsetA([10,7,22,14,28,17]));
+
